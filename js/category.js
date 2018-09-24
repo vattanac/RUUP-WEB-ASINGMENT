7be67d26f1a983e53ecf1b1eff005cd4752a92c0
vattanac <vattanacsim99@gmail.com>
2018-09-24 22:05:27 +0700

    update new page category detail

---
+++ js/category.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var myObj = JSON.parse(this.responseText);
+
+            for (var c in myObj.category) {
+                $(".mycarousel").append(mascard(myObj.category[c]));
+
+            }
+
+        }
+    };
+    xmlhttp.open("GET", "../Json/category.json", true);
+    xmlhttp.send();
+
+});
+
+
+function mascard(category) {
+    var c =
+        `<div class="carousel-cell " style="background-image: url('` + category.img + `'); background-size: 100% 100%;">
+            <div class="rgba-black-strong">
+
+                 <div class="text-white text-center" id="card-content">
+                      <div class="">
+                      <h3 class="card-title pt-2 fontMoulpali"><strong><a href="" style="color:#ffffff;">` + category.type + `</a></strong></h3>
+                     <div id="border1"></div>
+                      <p ><small>មាសមិនអាចកាត់ថ្លៃបាន</small></p>
+
+            </div>
+        </div>
+       
+    </div>
+</div>`
+    return c;
+}
